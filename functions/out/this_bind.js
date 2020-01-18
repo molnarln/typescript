d9ba17205a62d3a222ff3475d0f16537771c8a4f
Molnar Laszlo <laszlo.molnar25@gmail.com>
2020-01-18 18:22:40 +0100

    fix this_bind and this_arrow ts files

--- functions/out/this_bind.js
+++ functions/out/this_bind.js
@@ -3,15 +3,17 @@
         suits: ["hearts", "spades", "clubs", "diamonds"],
         cards: Array(52),
         createCardPicker: function () {
-            return () => {
+            console.log(`Outer this: ${this}`);
+            return function () {
                 let pickedCard = Math.floor(Math.random() * 52);
                 let pickedSuit = Math.floor(pickedCard / 13);
+                console.log(`Inner this: ${this}`);
                 return { suit: this.suits[pickedSuit], card: pickedCard % 13 };
             };
         }
     };
-    let cardPicker = deck.createCardPicker();
-    let pickedCard = cardPicker();
+    let cardPicker = deck.createCardPicker;
+    let pickedCard = cardPicker().bind(deck)();
     console.log("card: " + pickedCard.card + " of " + pickedCard.suit);
 })();
 //# sourceMappingURL=this_bind.js.map
--- functions/this_bind.ts
+++ functions/this_bind.ts
@@ -3,34 +3,19 @@
         suits: ["hearts", "spades", "clubs", "diamonds"],
         cards: Array(52),
         createCardPicker: function () {
-            return () => {
+            console.log(`Outer this: ${this}`);
+            return function () {
                 let pickedCard = Math.floor(Math.random() * 52);
                 let pickedSuit = Math.floor(pickedCard / 13);
-
+                console.log(`Inner this: ${this}`);
                 return { suit: this.suits[pickedSuit], card: pickedCard % 13 };
+
             }
         }
     }
 
-    let cardPicker = deck.createCardPicker();
-    let pickedCard = cardPicker();
+    let cardPicker = deck.createCardPicker;
+    let pickedCard = cardPicker().bind(deck)();
 
     console.log("card: " + pickedCard.card + " of " + pickedCard.suit);
 })()
-
-class Greeter2 {
-    greeting: string;
-    constructor(message: string) {
-        this.greeting = message;
-    }
-    greet() {
-        return () => {
-            console.log("Hello, " + this.greeting);
-        }
-    }
-}
-
-let g = new Greeter2("wow");
-
-let thing = g.greet();
-thing();
